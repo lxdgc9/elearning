bd6f67acfe127e276debb5bd2d961a1e14fdede7
DANH PHI LONG <longsf9@gmail.com>
2023-03-17 16:52:08 +0700

    catch invalid request

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
+import { NotFoundErr } from "./error/not-found";
 import { errHandler } from "./middleware/err-handler";
 import { authRouter } from "./router/auth";
 import { gpermRouter } from "./router/gperm";
@@ -20,6 +21,11 @@ app.use(userRouter);
 app.use(roleRouter);
 app.use(permRouter);
 app.use(gpermRouter);
+
+// Catch unknown request
+app.all("*", (_req, _res) => {
+  throw new NotFoundErr("REQ_NOT_FOUND");
+});
 
 app.use(errHandler);
 
