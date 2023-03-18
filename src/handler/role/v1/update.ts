860828994cc48178c426afdb7115f0203fd74b97
DANH PHI LONG <longsf9@gmail.com>
2023-03-18 08:12:24 +0700

    add description to role update

--- src/handler/role/v1/update.ts
+++ src/handler/role/v1/update.ts
@@ -4,6 +4,7 @@ import { Role } from "../../../model/role";
 
 type UpdateRoleDto = {
   name?: string;
+  description?: string;
   permissionIds?: Types.ObjectId[];
 };
 
@@ -13,13 +14,18 @@ async function updateRole(
   next: NextFunction
 ): Promise<void> {
   const { id } = req.params;
-  const { name, permissionIds }: UpdateRoleDto = req.body;
+  const {
+    name,
+    description,
+    permissionIds,
+  }: UpdateRoleDto = req.body;
 
   try {
     const role = await Role.findByIdAndUpdate(
       id,
       {
         name,
+        description,
         permissions: permissionIds,
       },
       { new: true }
