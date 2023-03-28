65c087db18fa102947bffe3029220ceaa1d575ca
DANH PHI LONG <longsf9@gmail.com>
2023-03-28 08:45:50 +0700

    update

--- src/sock/index.js
+++ src/sock/index.js
@@ -1,35 +1,22 @@
-// socket.js
 const socketio = require("socket.io");
 
-class Socket {
-  constructor(ws) {
-    this.io = new socketio.Server(ws, {
-      cors: {
-        origin: "*",
-      },
-    });
-    this.io.on("connection", (socket) => {
-      console.log("a socket connected", socket.id);
+function createSock(ws) {
+  const io = new socketio.Server(ws, {
+    cors: {
+      origin: "*",
+    },
+  });
 
-      socket.on("disconnect", () => {
-        console.log("a socket disconnected", socket.id);
-      });
-    });
-  }
+  console.log("Socket is starting!!!");
 
-  getInstance() {
-    return this.io;
-  }
+  io.on("connection", (socket) => {
+    console.log("a socket connected", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("a socket disconnected", socket.id);
+    });
+  });
+  app.locals.io = io;
 }
 
-let instance = null;
-
-function createSocket(ws) {
-  if (!instance) {
-    instance = new Socket(ws);
-  }
-
-  return instance;
-}
-
-module.exports = createSocket;
+module.exports = createSock;
