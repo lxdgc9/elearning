59e15739e1a56a8797156695fd3780f1fcbf639d
DANH PHI LONG <longsf9@gmail.com>
2023-04-12 08:59:35 +0700

    update

--- src/sock/index.js
+++ src/sock/index.js
@@ -14,6 +14,47 @@ function createSock(ws) {
   console.log("Socket is starting!!!");
 
   io.on("connection", (socket) => {
+    socket.on("join room", async (roomID, name) => {
+      //  Lấy roomID và username
+      socket.data.username = name;
+
+      // tạo peers với socket trong room
+      const peers = await io.in(roomID).fetchSockets();
+      if (peers.length === 5) {
+        socket.emit("room full");
+        return;
+      } // đặt giới hạn room
+
+      let members = []; // danh sách user trong room
+
+      await peers.forEach((peer) => {
+        members.push([peer.id, peer.data.username]);
+      });
+
+      socket.join(roomID);
+
+      socket.emit("getMembers", members);
+
+      socket.on("offer", (data) => {
+        socket.to(data.receiver).emit("user-joined", {
+          signal: data.signal,
+          sender: data.sender,
+          senderName: data.senderName,
+        });
+      });
+
+      socket.on("answer", (data) => {
+        socket.to(data.sender).emit("reconnect", {
+          signal: data.signal,
+          receiver: socket.id,
+        });
+      });
+
+      socket.on("leave room", () => {
+        socket.disconnect();
+      });
+    });
+
     socket.on("callUser", ({ signalData, from }) => {
       io.emit("callUser", { signal: signalData, from });
     });
