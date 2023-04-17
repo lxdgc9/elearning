5a17bf5f46660e68ffcc93289e34bfe739e243d8
DANH PHI LONG <longsf9@gmail.com>
2023-04-17 14:42:08 +0700

    .

--- src/handler/course/v1/update.js
+++ src/handler/course/v1/update.js
@@ -16,6 +16,7 @@ async function updateCourse(req, res, next) {
 
   let idxFile = 0;
   lessons.forEach((l) => {
+    cosnole.log(l, req.files[idxFile]);
     if (l.haveFile) {
       l.resource = req.files[idxFile]?.filename;
       idxFile = idxFile + 1;
